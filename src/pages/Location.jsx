76ee8dadcc30e6801d6f0495a177c1f48248d4e4
TuhinBanerjee31 <tuhinbanerjee0231@gmail.com>
2025-02-18 20:08:43 +0530

    modified locations page

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -4,30 +4,65 @@ import image1 from "../assets/locationImages/image1.jpg";
 import image11 from "../assets/SliderAreaImages/image1.jpg";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import image2 from "../assets/locationImages/image2.jpg";
+import image3 from "../assets/locationImages/image3.jpg";
+import image4 from "../assets/locationImages/image4.jpg";
+import image5 from "../assets/locationImages/image5.jpg";
+import image6 from "../assets/locationImages/image6.jpg";
+import image7 from "../assets/locationImages/image7.jpg";
+import image8 from "../assets/locationImages/image8.jpg";
+import image9 from "../assets/locationImages/image9.jpg";
+import image10 from "../assets/locationImages/image10.jpg";
+import image12 from "../assets/locationImages/image12.jpg";
+import image13 from "../assets/locationImages/image13.jpg";
+import image14 from "../assets/locationImages/image14.jpg";
+import image15 from "../assets/locationImages/image15.jpg";
+import image16 from "../assets/locationImages/image16.jpg";
+import image17 from "../assets/locationImages/image17.jpg";
+import image18 from "../assets/locationImages/image18.jpg";
+import image19 from "../assets/locationImages/image19.jpg";
+import image20 from "../assets/locationImages/image20.jpg";
+import image21 from "../assets/locationImages/image21.jpg";
+import image22 from "../assets/locationImages/image22.jpg";
+import image23 from "../assets/locationImages/image23.jpg";
+import image24 from "../assets/locationImages/image24.jpg";
+import image25 from "../assets/locationImages/image25.jpg";
+import image26 from "../assets/locationImages/image26.jpg";
+import image27 from "../assets/locationImages/image27.jpg";
+import image28 from "../assets/locationImages/image28.jpg";
+
 
 const Location = () => {
   const asia = [
     {
       id: 1,
       state: "Visakhapatnam, India",
+      office1: image2,
+      office2: image3,
       address:
         "KPNG Office, 1st Floor, HMDA Complex, Dwaraka Nagar, Main Road, Visakhapatnam, Andhra Pradesh, 530016, India",
     },
     {
       id: 2,
       state: "Singapore, Singapore",
+      office1: image4,
+      office2: image5,
       address:
         "KPNG Office, 1 Raffles Place, #14-05, One Raffles Place, Singapore 048616",
     },
     {
       id: 3,
       state: "Shanghai, China",
+      office1: image6,
+      office2: image7,
       address:
         "KPNG Office, 5th Floor, The Center, 989 Changle Road, Xuhui District, Shanghai, 200031, China",
     },
     {
       id: 4,
       state: "Moscow, Russia",
+      office1: image8,
+      office2: image9,
       address:
         "KPNG Office, 10th Floor, Business Center 'Vernadsky', 15 Vernadskogo Avenue, Moscow, 119602, Russia",
     },
@@ -37,18 +72,24 @@ const Location = () => {
     {
       id: 1,
       state: "London, United Kingdom",
+      office1: image10,
+      office2: image12,
       address:
         "KPNG Office, 4th Floor, 70 Gracechurch Street, London, EC3V 0HR, United Kingdom",
     },
     {
       id: 2,
       state: "Berlin, Germany",
+      office1: image13,
+      office2: image14,
       address:
         "KPNG Office, 3rd Floor, Potsdamer Platz 10, 10785 Berlin, Germany",
     },
     {
       id: 3,
       state: "Paris, France",
+      office1: image15,
+      office2: image16,
       address: "KPNG Office, 33 Rue de la Bourse, 75002 Paris, France",
     },
   ];
@@ -57,18 +98,24 @@ const Location = () => {
     {
       id: 1,
       state: "New York City, United States",
+      office1: image17,
+      office2: image18,
       address:
         "KPNG Office, 7th Floor, 285 Madison Avenue, New York, NY 10017, United States",
     },
     {
       id: 2,
       state: "Toronto, Canada",
+      office1: image19,
+      office2: image20,
       address:
         "KPNG Office, Suite 500, 123 Front Street West, Toronto, ON M5J 2M2, Canada",
     },
     {
       id: 3,
       state: "Los Angeles, United States",
+      office1: image21,
+      office2: image22,
       address:
         "KPNG Office, Suite 400, 1000 Wilshire Blvd, Los Angeles, CA 90017, United States",
     },
@@ -78,6 +125,8 @@ const Location = () => {
     {
       id: 1,
       state: "Johannesburg, South Africa",
+      office1: image23,
+      office2: image24,
       address:
         "KPNG Office, 5th Floor, The Campus, 57 Sloane Street, Bryanston, Johannesburg, 2191, South Africa",
     },
@@ -87,12 +136,16 @@ const Location = () => {
     {
       id: 1,
       state: "São Paulo, Brazil",
+      office1: image25,
+      office2: image26,
       address:
         "KPNG Office, Avenida Paulista, 1234, 5th Floor, Bela Vista, São Paulo, SP, 01310-100, Brazil",
     },
     {
       id: 2,
       state: "Buenos Aires, Argentina",
+      office1: image27,
+      office2: image28,
       address:
         "KPNG Office, Avenida 9 de Julio, 800, 12th Floor, Retiro, Buenos Aires, C1009AAQ, Argentina",
     },
@@ -147,7 +200,7 @@ const Location = () => {
         </div>
 
         {/* Accordion */}
-        <div className="max-w-2xl mx-auto px-4">
+        <div className="max-w-screen-lg mx-auto px-4">
           {data.map((item) => (
             <div key={item.id} className="mb-4">
               <h2 id={`accordion-heading-${item.id}`}>
@@ -184,10 +237,14 @@ const Location = () => {
                 }`}
                 aria-labelledby={`accordion-heading-${item.id}`}
               >
-                <div className="p-4 md:p-6 bg-gray-100 rounded-b-lg">
+                <div className="p-4 md:p-6 bg-gray-100 rounded-b-lg flex flex-col-reverse md:flex-row gap-5 md:gap-0 justify-center md:justify-around items-center">
                   <p className="text-gray-600 max-w-sm text-base md:text-lg">
                     {item.address}
                   </p>
+                  <div className="flex gap-2">
+                    <img src={item.office1} className="h-24 w-auto" />
+                    <img src={item.office2} className="h-24 w-auto" />
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -1,18 +1,90 @@
 import Cheif from "../components/Cheif";
 import CustomHeader from "../components/CustomHeader";
 import Footer from "../components/Footer";
+import image1 from "../assets/ourWorkImages/image1.jpg";
+import image2 from "../assets/ourWorkImages/image2.jpg";
+import { Link } from "react-router-dom";
+import image11 from "../assets/SliderAreaImages/image1.jpg";
 
 const WorkPage = () => {
   return (
     <>
       <CustomHeader
-        bgImg={
-          "https://images.unsplash.com/photo-1651195297119-afc97f14a40d?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }
-        boldLine={"The way we build"}
-        normalLine={"Makes sure to leave a positive impact."}
+        bgImg={image1}
+        boldLine={"Our Work"}
+        normalLine={"Most Reliable Infrastructure"}
       />
       <Cheif />
+      <div className="py-10 bg-gray-900">
+        <div className="flex items-center relative">
+          <div className="container mx-auto py-8 grid grid-cols-1 lg:grid-cols-2">
+            {/* Image Section */}
+            <div className="p-5 md:p-20">
+              <img
+                src={image2}
+                alt="Collaborative Partner"
+                className="w-full h-full object-cover shadow"
+              />
+            </div>
+
+            {/* Text Section */}
+            <div className="max-w-screen-md h-full">
+              <div className="h-full text-white p-8 md:rounded-lg flex flex-col items-center justify-center">
+                <h3 className="font-light text-5xl md:text-7xl text-center uppercase">
+                  MARKETS WE SERVE NATIONALLY
+                </h3>
+                <p className="font-light text-xl max-w-screen-sm tracking-wider text-center py-8">
+                  We are deeply committed to the communities we serve,
+                  recognizing that our work goes beyond construction—it’s about
+                  building connections, fostering growth, and creating lasting
+                  positive impacts. Every project we undertake is driven by the
+                  belief that strong communities are the foundation of a
+                  thriving society. We actively engage with local stakeholders,
+                  invest in sustainable practices, and support initiatives that
+                  enhance the well-being of the people around us. From creating
+                  job opportunities to supporting community programs, our goal
+                  is to leave a legacy of progress and prosperity. Together, we
+                  strive to build not just structures, but resilient, inclusive,
+                  and vibrant communities for generations to come.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center relative py-28">
+        <div className="container mx-auto py-8 grid grid-cols-1 lg:grid-cols-2">
+          {/* Image Section */}
+          <div>
+            <img
+              src={image11}
+              alt="Collaborative Partner"
+              className="w-full h-full object-cover shadow"
+            />
+          </div>
+
+          {/* Text Section */}
+          <div className="md:absolute max-w-screen-md right-[20%] top-[25%]">
+            <div className="bg-gray-900 text-white p-8 md:rounded-lg flex flex-col justify-center">
+              <h2 className="text-3xl sm:text-4xl lg:text-5xl font-light mb-4">
+                Let’s Talk
+              </h2>
+              <h2 className="text-3xl sm:text-4xl uppercase lg:text-5xl font-light mb-4">
+                Let’s Build Together
+              </h2>
+              <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6">
+                Need to Reach Us?
+              </h3>
+              <div>
+                <Link to={"/contact"} className="button-48 py-10">
+                  <span className="text">CONTACT US</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
