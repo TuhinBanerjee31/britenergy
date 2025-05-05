255b07168e58b3b6ab0953d7b10465eaa77f0f68
TuhinBanerjee31 <tuhinbanerjee0231@gmail.com>
2025-05-05 19:44:42 +0530

    modified content

--- src/components/SpecialDisplay.jsx
+++ src/components/SpecialDisplay.jsx
@@ -2,22 +2,26 @@ import Marquee from "react-fast-marquee";
 
 const SpecialDisplay = () => {
   return (
-    <Marquee speed={60} className="bg-[#1C78B6] py-1 font-roboto-condensed flex text-white text-lg tracking-widest">
+    <Marquee
+      speed={60}
+      className="bg-[#1C78B6] py-1 font-roboto-condensed flex text-white text-lg tracking-widest"
+    >
       <h5 className="px-2">৹</h5>
       <h5>
-        K.P.N.G (Knagz Power and Network-Infrastructure Group) is working as a
-        subcontractor on different projects throughout the India in
-        collaboration with the MoHUA (Ministry Housing and Urban Affairs)
+        K.P.N.G (Knagz Power and Network-Infrastructure Group) operates as a
+        subcontractor on various projects across India in collaboration with the
+        Ministry of Housing and Urban Affairs (MoHUA) and the Government of
+        India
       </h5>
       <h5 className="px-2">৹</h5>
       <h5>
-        Job applicants are advised to have advance professional skill
-        certification available before placing job application
+        Job applicants are advised to have advanced professional skill
+        certification available before submitting their job application
       </h5>
       <h5 className="px-2">৹</h5>
       <h5>
-        K.P.N.G (Knagz Power and Network-Infrastructure Group) never ask for
-        money for any kind of job opportunities
+        K.P.N.G (Knagz Power and Network-Infrastructure Group) never requests
+        payment for any job opportunities
       </h5>
     </Marquee>
   );
